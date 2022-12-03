7085159874f0873aace1a5cf6de9cd0e02c77f8a
Tetsui Ohkubo <peryaudo@gmail.com>
2022-12-04 01:58:58 +0900

    Add BitReader

--- gunziptest.cc
+++ gunziptest.cc
@@ -1,9 +1,30 @@
+// Based on https://cs.opensource.google/go/go/+/38801e55dbdd19d69935b92e38b1a4c9949316bf:src/lib/compress/flate/inflate.go;bpv=0
+
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 
 #include <cstdint>
 #include <cstdio>
+
+class BitReader {
+public:
+    BitReader(uint8_t* data) : data_(data) {}
+    uint32_t Read(uint32_t requested_count) {
+        if (available_count_ < requested_count) {
+            bytes_ += (uint32_t)(data_[0]) << available_count_;
+            ++data_;
+            available_count_ += 8;
+        }
+        uint32_t result = bytes_ & ((1 << requested_count) - 1);
+        bytes_ >>= requested_count;
+        available_count_ -= requested_count;
+        return result;
+    }
+    uint32_t bytes_ = 0;
+    uint32_t available_count_ = 0;
+    uint8_t* data_;
+};
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -41,7 +62,9 @@ int main(int argc, char* argv[]) {
         while (*data) ++data;
         ++data;
     }
-    uint8_t byte = data[0];
-    printf("final? = %s type = %d\n", (byte & 1 ? "yes" : "no"), (byte & 6) >> 1);
+    BitReader reader(data);
+    uint32_t final = reader.Read(1);
+    uint32_t type = reader.Read(2);
+    printf("final? = %s type = %d\n", (final ? "yes" : "no"), type);
     return 0;
 }
